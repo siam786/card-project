996b66af6a2e0a12f5dd131b27a3e76e09593097
siam786 <shohrab.hossain36@gmail.com>
2021-07-04 01:11:25 +0600

    final project

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const panels = document.querySelectorAll(".panel");
 
-console.log(panels[2]);
-panels.forEach((panel) => {
+
+panels.forEach(panel => {
   panel.addEventListener("click", () => {
+    removeActiveClass()
       panel.classList.add('active')
   });
 });
+
+function removeActiveClass() { 
+panels.forEach(panel =>{
+  panel.classList.remove('active')
+})
+}
