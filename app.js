b72a9732e27a6b1d6f150f8b32eebfb8864262d6
siam786 <shohrab.hossain36@gmail.com>
2021-07-04 01:16:23 +0600

    add commets

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+//grap panel class
 const panels = document.querySelectorAll(".panel");
 
 
 panels.forEach(panel => {
   panel.addEventListener("click", () => {
     removeActiveClass()
+    //add active class
       panel.classList.add('active')
   });
 });
 
+//write remove class
 function removeActiveClass() { 
 panels.forEach(panel =>{
   panel.classList.remove('active')
